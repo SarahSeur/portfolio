07eda1a9e958080bd9b1b77d62b6b468f9ef61cd
Sarah Seur <sarahseur@hotmail.com>
2025-05-05 10:14:28 +0200

    Add files via upload

---
+++ script/index.js
@@ -0,0 +1,17 @@
+const horizontalSwiper = new Swiper(".card-swiper--horizontal", {
+  slidesPerView: "auto",
+  spaceBetween: 125,
+  freeMode: true,
+  grabCursor: true,
+});
+
+const verticalSwiper = new Swiper(".card-swiper--vertical", {
+  direction: 'vertical',
+  effect: "cards",
+  initialSlide: 1,
+  grabCursor: true,
+  cardsEffect: {
+    perSlideOffset: 20,
+    perSlideRotate: 0,
+  },
+});
